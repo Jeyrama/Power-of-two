9926ab2501b1f1f88f522b7fff7fa077c27ff41f
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-02 09:58:10 -0700

    Initial commit

---
+++ Power-of-two.js
@@ -0,0 +1,14 @@
+/*
+Complete the function powerOfTwo that determines 
+if a given non-negative integer is a power of two.
+You may assume the input is always valid.
+
+Examples:
+isPowerOfTwo(1024) // -> true
+isPowerOfTwo(4096) // -> true
+isPowerOfTwo(333)  // -> false
+
+Beware of certain edge cases - for example, 
+1 is a power of 2 since 2^0 = 1 and 0 is not a power of 2.
+*/
+
